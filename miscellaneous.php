8f2f9aa5d8b4eb8114d95d18c1a036dc837a7af3
SrStick <gabrielrsep@hotmail.com>
2024-09-08 21:01:26 -0300

    create createdOptions function

--- miscellaneous.php
+++ miscellaneous.php
@@ -4,3 +4,15 @@ namespace Underline\Miscellaneous;
 function echoExeption(\Throwable $throwable) {
   echo $throwable->getCode() . ' ' . $throwable->getMessage();
 }
+
+function createOptions(array $options, $selectedValue = null) {
+  $out = '';
+  foreach ($options as $label => $value) {
+    $out .= "<option value='{$value}'";
+    if(is_callable($selectedValue) && $selectedValue($value) || $value === $selectedValue) {
+      $out .= ' selected';
+    }
+    $out .= ">{$label}</option>";
+  }
+  return $out;
+}
